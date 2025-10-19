a86892b945f782da12a74feb4c0677e0a97de978
Minh Ngo <130447795+minhngosiukute@users.noreply.github.com>
2025-10-19 15:09:08 +0700

    xoaviewthongke

--- WebBanHangOnline/Global.asax.cs
+++ WebBanHangOnline/Global.asax.cs
@@ -18,13 +18,7 @@ namespace WebBanHangOnline
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
-            Application["HomNay"] = 0;
-            Application["HomQua"] = 0;
-            Application["TuanNay"] = 0;
-            Application["TuanTruoc"] = 0;
-            Application["ThangNay"] = 0;
-            Application["ThangTruoc"] = 0;
-            Application["TatCa"] = 0;
+            
             Application["visitors_online"] = 0;
         }
         void Session_Start(object sender, EventArgs e)
@@ -39,22 +33,7 @@ namespace WebBanHangOnline
             Application.Lock();
             Application["visitors_online"] = Convert.ToInt32(Application["visitors_online"]) + 1;
             Application.UnLock();
-            try
-            {
-                var item = WebBanHangOnline.Models.Common.ThongKeTruyCap.ThongKe();
-                if (item!=null)
-                {
-                    Application["HomNay"] = long.Parse("0" + item.HomNay.ToString("#,###"));
-                    Application["HomQua"] = long.Parse("0" + item.HomQua.ToString("#,###"));
-                    Application["TuanNay"] = long.Parse("0" + item.ThangNay.ToString("#,###"));
-                    Application["TuanTruoc"] = long.Parse("0" + item.ThangTruoc.ToString("#,###"));
-                    Application["ThangNay"] = long.Parse("0" + item.ThangNay.ToString("#,###"));
-                    Application["ThangTruoc"] = long.Parse("0" + item.ThangTruoc.ToString("#,###"));
-                    Application["TatCa"] = (int.Parse(item.TatCa.ToString())).ToString("#,###");
-                }
-               
-            }
-            catch { }
+            
 
         }
         void Session_End(object sender, EventArgs e)
