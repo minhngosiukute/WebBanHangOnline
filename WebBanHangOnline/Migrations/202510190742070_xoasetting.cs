518109a72e9c595780a4fbda61981a676651d0a8
Minh Ngo <130447795+minhngosiukute@users.noreply.github.com>
2025-10-19 15:03:28 +0700

    xoacataseting

---
+++ WebBanHangOnline/Migrations/202510190742070_xoasetting.cs
@@ -0,0 +1,27 @@
+﻿namespace WebBanHangOnline.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class xoasetting : DbMigration
+    {
+        public override void Up()
+        {
+            DropTable("dbo.tb_SystemSetting");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.tb_SystemSetting",
+                c => new
+                    {
+                        SettingKey = c.String(nullable: false, maxLength: 50),
+                        SettingValue = c.String(maxLength: 4000),
+                        SettingDescription = c.String(maxLength: 4000),
+                    })
+                .PrimaryKey(t => t.SettingKey);
+            
+        }
+    }
+}
--- WebBanHangOnline/Models/IdentityModels.cs
+++ WebBanHangOnline/Models/IdentityModels.cs
@@ -31,16 +31,16 @@ namespace WebBanHangOnline.Models
         public DbSet<Wishlist> Wishlists { get; set; }
         public DbSet<ReviewProduct> Reviews { get; set; }
         public DbSet<ThongKe> ThongKes { get; set; }
-        public DbSet<Category> Categories { get; set; }
+      
         public DbSet<Adv> Advs { get; set; }
         public DbSet<AccessStat> AccessStats { get; set; }
 
         public DbSet<News> News { get; set; }
-        public DbSet<SystemSetting> SystemSettings { get; set; }
+        
         public DbSet<ProductCategory> ProductCategories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductImage> ProductImages { get; set; }
-        public DbSet<Contact> Contacts { get; set; }
+       
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderDetail> OrderDetails { get; set; }
         public DbSet<Subscribe> Subscribes { get; set; }
