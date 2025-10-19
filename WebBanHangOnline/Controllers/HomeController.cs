9a40872f280ab1893d5c131c8623e0196bee67dd
Minh Ngo <130447795+minhngosiukute@users.noreply.github.com>
2025-10-19 13:45:09 +0700

    xoatruycap

--- WebBanHangOnline/Controllers/HomeController.cs
+++ WebBanHangOnline/Controllers/HomeController.cs
@@ -61,21 +61,7 @@ namespace WebBanHangOnline.Controllers
 
             return View();
         }
-        public ActionResult Refresh()
-        {
-            var item = new ThongKeModel();
-
-            ViewBag.Visitors_online = HttpContext.Application["visitors_online"];
-            var hn = HttpContext.Application["HomNay"];
-            item.HomNay = HttpContext.Application["HomNay"].ToString();
-            item.HomQua = HttpContext.Application["HomQua"].ToString();
-            item.TuanNay = HttpContext.Application["TuanNay"].ToString();
-            item.TuanTruoc = HttpContext.Application["TuanTruoc"].ToString();
-            item.ThangNay = HttpContext.Application["ThangNay"].ToString();
-            item.ThangTruoc = HttpContext.Application["ThangTruoc"].ToString();
-            item.TatCa = HttpContext.Application["TatCa"].ToString();
-            return PartialView(item);
-        }
+        
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
