d84c80b3c909acfc85396c07039d4bf33fb35fa1
Minh Ngo <130447795+minhngosiukute@users.noreply.github.com>
2025-11-01 18:03:45 +0700

    xoacata

---
+++ WebBanHangOnline/Migrations/202510311825533_xoaproducca.cs
@@ -0,0 +1,20 @@
+﻿namespace WebBanHangOnline.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class xoaproducca : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.tb_ProductCategory", "Description");
+            DropColumn("dbo.tb_ProductCategory", "Icon");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.tb_ProductCategory", "Icon", c => c.String(maxLength: 250));
+            AddColumn("dbo.tb_ProductCategory", "Description", c => c.String());
+        }
+    }
+}
