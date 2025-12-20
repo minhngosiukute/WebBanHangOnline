b5110f48a060b30ff12906c50341830b2be2a5bb
Minh Ngo <130447795+minhngosiukute@users.noreply.github.com>
2025-12-21 03:22:45 +0700

    chuyen gio hang

---
+++ WebBanHangOnline/Models/EF/Cart.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebBanHangOnline.Models;
+
+namespace WebBanHangOnline.Models.EF
+{
+    [Table("tb_Cart")]
+    public class Cart : CommonAbstract
+    {
+        public Cart()
+        {
+            Items = new HashSet<CartItem>();
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        public virtual ICollection<CartItem> Items { get; set; }
+    }
+}
---
+++ WebBanHangOnline/Models/EF/CartItem.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebBanHangOnline.Models.EF
+{
+    [Table("tb_CartItem")]
+    public class CartItem
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int CartId { get; set; }
+
+        public int ProductId { get; set; }
+
+        [Required]
+        public string ProductName { get; set; }
+
+        public string Alias { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string ProductImg { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+        [ForeignKey("CartId")]
+        public virtual Cart Cart { get; set; }
+    }
+}
